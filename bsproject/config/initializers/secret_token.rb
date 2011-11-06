214dda516974a72b632793d33c81491d2282c3a8
Jonghyun Thom <ljh0875@gmail.com>
2011-11-06 14:29:23 -0500

    second commit

---
+++ bsproject/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bsproject::Application.config.secret_token = '4f3da1ac880ffa9f7214b4cd6f3733a99f563d4d733a9f5956207b94cdfdbe059f753c993f0256a7e3805a9b4f3867692ffc13d04e1bbc24e5dc6e4a1ae03ffc'
